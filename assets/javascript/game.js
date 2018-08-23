488a53e3fb7fe550038aeff5b34d25d4bc269b87
rwon@corp.drillmap.com <rwon@corp.drillmap.com>
2018-08-22 23:36:36 -0500

    add pictures

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -34,3 +34,31 @@ function findDuplicateElement(array, element) {
     return duplicates[element];
 }
 
+// main program
+
+$(document).ready(function () {
+
+    var letters = ["A", "B", "C", "D"];
+
+    for (var i = 0; i < letters.length; i++) {
+
+        var letterBtn = $("<button>");
+        letterBtn.addClass("letter-button letter letter-button-color");
+        letterBtn.attr("data-letter", letters[i]);
+        letterBtn.text(letters[i]);
+        $("#buttons").append(letterBtn);
+    }
+
+    $(".letter-button").on("click", function () {
+        var fridgeMagnet = $("<div>");
+        fridgeMagnet.addClass("letter fridge-color");
+        fridgeMagnet.text($(this).attr("data-letter"));
+        $("#display").append(fridgeMagnet);
+
+    });
+
+    $("#clear").on("click", function () {
+        $("#display").empty();
+    });
+
+});
