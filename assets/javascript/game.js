18b6bbf25e671c3bd06dffef7f2a8cae3c2c1b82
rwon@corp.drillmap.com <rwon@corp.drillmap.com>
2018-08-24 18:21:27 -0500

    fix the instruction.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -47,34 +47,8 @@ $(document).ready(function () {
             $("#buttons").append(letterBtn);
         }
     }
-    
 
 
-    //--------------------------------------------------
-
-
-    // var letters = ["A", "B", "C", "D"];
-
-    // for (var i = 0; i < letters.length; i++) {
-
-    //     var letterBtn = $("<button>");
-    //     letterBtn.addClass("letter-button letter letter-button-color");
-    //     letterBtn.attr("data-letter", letters[i]);
-    //     letterBtn.text(letters[i]);
-    //     $("#buttons").append(letterBtn);
-    // }
-
-    // $(".letter-button").on("click", function () {
-    //     var fridgeMagnet = $("<div>");
-    //     fridgeMagnet.addClass("letter fridge-color");
-    //     fridgeMagnet.text($(this).attr("data-letter"));
-    //     $("#display").append(fridgeMagnet);
-
-    // });
-
-    // $("#clear").on("click", function () {
-    //     $("#display").empty();
-    // });
 
     //---------------------------------------------------------------------
     // let letters = ["Doctor", "Dalek", "Cyberman", "The Master", "Rose Tyler", "Captain Jack"];
@@ -92,12 +66,13 @@ $(document).ready(function () {
     //---------------------------------------------------------------------
     // Person object properties:
     //   1. name = name of the charater 
+    //   2. shortname = short name for the character, match the variable name
     //   2. health = character's health point
     //   3. attackPoint = when attacking, points that will reduce the opponent's health
     //   4. counter_attackPoint = when defending, points that will reduce the opponent's health
     //   5. isFriendly - true for the good guys, false for the bad guys.
 
-    let doctor = new Person('The Doctor', 'doctor', 1000, 250, 150, true);
+    let doctor = new Person('The Doctor', 'doctor', 1500, 280, 150, true);
     let dalek = new Person('Dalek', 'dalek', 200, 200, 50, false);
     let cyberman = new Person('Cyberman', 'cyberman', 500, 100, 100, false);
     let master = new Person('The Master', 'master', 1200, 300, 150, false);
@@ -110,14 +85,14 @@ $(document).ready(function () {
     let name = '';
     let choosePlayer = '';
     let isDefenderChosen = false;
-    
+
     let attacker = '';
     let defender = '';
     let isGameStarted = false;
     let isBattleEnded = false;
     let playernames = [];
     let friendlyList = [];
-    
+
     // win or lose will be implemented by a random generator
     // let wins = [1, 0, 1, 1, 1, 1];
     // let win = Math.floor(Math.random() * 2);
@@ -156,44 +131,44 @@ $(document).ready(function () {
 
         console.log(choosePlayer);
 
-        // choosing attacker
+        // choose attacker
         if (!attacker) {
 
             attacker = choosePlayer;
             removeElement(players, attacker);
-           
+
 
             // remove friendly in players
             for (let i = 0; i < players.length; i++) {
-            
+
                 if (attacker.isFriendly === players[i].isFriendly) {
                     friendlyList.push(players[i].shortname);
-            
+
                 }
             }
-          
+
             for (let i = 0; i < friendlyList.length; i++) {
                 if (friendlyList[i] === 'roseTyler') {
                     removeElement(players, roseTyler);
-                } 
+                }
                 if (friendlyList[i] === 'captainJack') {
                     removeElement(players, captainJack);
-                } 
+                }
                 if (friendlyList[i] === 'doctor') {
                     removeElement(players, doctor);
-                } 
+                }
                 if (friendlyList[i] === 'master') {
                     removeElement(players, master);
-                } 
+                }
                 if (friendlyList[i] === 'dalek') {
                     removeElement(players, dalek);
-                } 
+                }
                 if (friendlyList[i] === 'cyberman') {
                     removeElement(players, cyberman);
-                } 
+                }
             }
-            
-          
+
+
             console.log(friendlyList);
             console.log(players);
 
@@ -204,8 +179,9 @@ $(document).ready(function () {
             // -- printout
             $(".first-number").text("You are " + attacker.name + ".    [health point = " + attacker.health + "]");
             console.log("attacker is " + attacker.name);
-            $(".instruction").text("Choose Your Opponent: "  + playernames);
-            $(".instruction2").text("");
+            $(".instruction").text("Choose Your Opponent: ");
+            $(".instruction2").text(playernames);
+            $(".instruction3").text("");
             $(".result").text("Opponents Left Are:     " + playernames);
         }
 
@@ -233,12 +209,13 @@ $(document).ready(function () {
             removeElement(playernames, defender.name);
             isBattleEnded = false;
             isDefenderChosen = true;
-          
+
             // wins = generateRandomNumberArray(2, 20);
 
             // -- printout
             $(".instruction").text("Click Attack Button To Fight.");
             $(".instruction2").text("");
+            $(".instruction3").text("");
             $(".second-number").text("Your Opponent is " + defender.name + ".    [health point = " + defender.health + "]");
             $(".result").text("Opponents Left Are:     " + playernames);
             console.log("defender is " + defender.name);
@@ -271,17 +248,17 @@ $(document).ready(function () {
             if (win) {
                 win_count++;
                 battle(attacker, defender, win_count);
-                $(".instruction").text("Win. Click Attack Button To Fight Again.");
+                $(".instruction").text("Win This Round. Click Attack Button To Fight Again.");
                 console.log("inside attacker=" + attacker.name + " " + attacker.health);
                 console.log("inside defender=" + defender.name + " " + defender.health);
             }
             else {
                 battle(defender, attacker, 1);
-                $(".instruction").text("Lose. Click Attack Button To Fight Again.");
+                $(".instruction").text("Lose This Round. Click Attack Button To Fight Again.");
             }
 
             //-- print out
-           
+
             $(".first-number").text("You are " + attacker.name + ".    [health point = " + attacker.health + "]");
             $(".second-number").text("Your Opponent is " + defender.name + ".    [health point = " + defender.health + "]");
             console.log("defender is " + defender.name);
@@ -289,16 +266,24 @@ $(document).ready(function () {
             console.log("final defender=" + defender.name + " " + defender.health);
         }
 
-        if(isGameStarted && ( attacker.health < 1 || defender.health < 1)) {
+        if (isGameStarted && (attacker.health < 1 || defender.health < 1)) {
             isBattleEnded = true;
             isDefenderChosen = false;
+            console.log("final = " + playernames);
             if (attacker.health < 1) {
                 $(".instruction").text("You Lose. " + attacker.name + " is dead.");
-                $(".instruction2").text("Click Refresh in Browser To Restart The Game.");
+                $(".instruction2").text("Refresh in Browser To Restart The Game.");
+                $(".instruction3").text("");
             }
-            else {
+            else if (playernames.length > 1) {
                 $(".instruction").text("You Win. " + defender.name + " is dead.");
-                $(".instruction2").text("Choose Your Next Opponent: " + playernames);
+                $(".instruction2").text("Choose Your Next Opponent: ");
+                $(".instruction3").text(playernames);
+            }
+            else if (playernames.length < 1) {
+                $(".instruction").text("You Win. " + defender.name + " is dead.");
+                $(".instruction2").text("Refresh in Browser To Restart The Game.");
+                $(".instruction3").text("");
             }
 
         }
