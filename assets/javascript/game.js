b290a2ca98d83cce74bd5834615878de8f194beb
rwon <rwon>
2018-08-23 21:12:25 -0500

    continue working on event listener

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,68 +1,68 @@
 $(document).ready(function () {
 
-// This function remove element from an array
-function removeElement(array, element) {
-    let index = array.indexOf(element);
-    if (index > -1) {
-        array.splice(index, 1);
-    }
-}
-
-// This function counts element occurances in an array
-function countElement(array, element) {
-    let counts = {};
-    for (var i = 0; i < array.length; i++) {
-        if (!counts.hasOwnProperty(array[i])) {
-            counts[array[i]] = 1;
-        }
-        else {
-            counts[array[i]]++;
+    // This function remove element from an array
+    function removeElement(array, element) {
+        let index = array.indexOf(element);
+        if (index > -1) {
+            array.splice(index, 1);
         }
     }
-    return counts[element];
-}
 
-// This function finds index of duplicate elements in an array
-function findDuplicateElement(array, element) {
-    let duplicates = {};
-    for (var i = 0; i < array.length; i++) {
-        if (duplicates.hasOwnProperty(array[i])) {
-            duplicates[array[i]].push(i);
+    // This function counts element occurances in an array
+    function countElement(array, element) {
+        let counts = {};
+        for (var i = 0; i < array.length; i++) {
+            if (!counts.hasOwnProperty(array[i])) {
+                counts[array[i]] = 1;
+            }
+            else {
+                counts[array[i]]++;
+            }
         }
-        else if (array.lastIndexOf(array[i]) !== i) {
-            duplicates[array[i]] = [i];
-        }
+        return counts[element];
     }
-    return duplicates[element];
-}
 
-// Object for player
-function Person(name, health, attackPoint, counter_attackPoint, isFriendly) {
-    this.name = name;
-    this.health = health;
-    this.attackPoint = attackPoint;
-    this.counter_attackPoint = counter_attackPoint;
-    this.isFriendly = isFriendly;
+    // This function finds index of duplicate elements in an array
+    function findDuplicateElement(array, element) {
+        let duplicates = {};
+        for (var i = 0; i < array.length; i++) {
+            if (duplicates.hasOwnProperty(array[i])) {
+                duplicates[array[i]].push(i);
+            }
+            else if (array.lastIndexOf(array[i]) !== i) {
+                duplicates[array[i]] = [i];
+            }
+        }
+        return duplicates[element];
+    }
 
-    this.attacking = function (defenderPoint) {
-        this.health -= defenderPoint;
-        // console.log(this.name + " " + defenderPoint + "health" + this.health);
-    };
+    // Object for player
+    function Person(name, health, attackPoint, counter_attackPoint, isFriendly) {
+        this.name = name;
+        this.health = health;
+        this.attackPoint = attackPoint;
+        this.counter_attackPoint = counter_attackPoint;
+        this.isFriendly = isFriendly;
 
-    this.defending = function (attackerPoint, multiple) {
-        this.health -= (attackerPoint * multiple);
-        // console.log(this.name + " " + attackerPoint + "health" + this.health);
-    };
-}
+        this.attacking = function (defenderPoint) {
+            this.health -= defenderPoint;
+            // console.log(this.name + " " + defenderPoint + "health" + this.health);
+        };
 
-// function for battle
-function battle(winner, loser, multiple) {
-    winner.attacking(loser.counter_attackPoint);
-    loser.defending(winner.attackPoint, multiple);
-}
+        this.defending = function (attackerPoint, multiple) {
+            this.health -= (attackerPoint * multiple);
+            // console.log(this.name + " " + attackerPoint + "health" + this.health);
+        };
+    }
+
+    // function for battle
+    function battle(winner, loser, multiple) {
+        winner.attacking(loser.counter_attackPoint);
+        loser.defending(winner.attackPoint, multiple);
+    }
 
 
-// main program
+    // main program
 
 
     // var letters = ["A", "B", "C", "D"];
@@ -88,13 +88,13 @@ function battle(winner, loser, multiple) {
     //     $("#display").empty();
     // });
 
-   //---------------------------------------------------------------------
-   // Person object properties:
-   //   1. name = name of the charater 
-   //   2. health = character's health point
-   //   3. attackPoint = when attacking, points that will reduce the opponent's health
-   //   4. counter_attackPoint = when defending, points that will reduce the opponent's health
-   //   5. isFriendly - true for the good guys, false for the bad guys.
+    //---------------------------------------------------------------------
+    // Person object properties:
+    //   1. name = name of the charater 
+    //   2. health = character's health point
+    //   3. attackPoint = when attacking, points that will reduce the opponent's health
+    //   4. counter_attackPoint = when defending, points that will reduce the opponent's health
+    //   5. isFriendly - true for the good guys, false for the bad guys.
 
     let doctor = new Person('The Doctor', 1000, 250, 150, true);
     let dalek = new Person('Dalek', 200, 200, 50, false);
@@ -106,18 +106,41 @@ function battle(winner, loser, multiple) {
     let players = [doctor, dalek, cyberman, master, roseTyler];
 
     let firstNumber = '';
+  
+
+console.log("find it" + players.includes(doctor));
 
     $(".number").on("click", function () {
         console.log($(this).val());
-        firstNumber += $(this).val();
-        $("#first-number").text("Attacker is " + firstNumber);
-        console.log(firstNumber);
+       
+            console.log("no");
+            firstNumber = $(this).val();
+            $("#first-number").text("Attacker is " + firstNumber);
+            console.log("attacker is " + firstNumber);
+ 
+      
+            console.log(players);            
+           
+        
+        
+
     });
 
+    let attacker_tmp = '';
+    
+    if (firstNumber === 'dalek') {
+        console.log("first = " + firstNumber);
+        attacker_tmp = dakek;
+        console.log("tmp inside =" + attacker_tmp); 
+     }
+    console.log("tmp=" + attacker_tmp); 
     // test cases:
-     let attacker = doctor;
-    // let attacker = roseTyler ;
-     // -------------------------
+    let attacker = doctor;
+    // attacker = roseTyler ;
+    // -------------------------
+
+
+
 
     removeElement(players, attacker);
 
@@ -126,12 +149,12 @@ function battle(winner, loser, multiple) {
 
     // ---
     let win_count = 0;
-    
+
     for (let i = 0; i < players.length; i++) {
         let defender = players[i];
         console.log("before attacker=" + attacker.name + " " + attacker.health);
         console.log("before defender=" + defender.name + " " + defender.health);
-        if (attacker.health < 1 ) {
+        if (attacker.health < 1) {
             break;
         }
         for (let j = 0; j < wins.length; j++) {
